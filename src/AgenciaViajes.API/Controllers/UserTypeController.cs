8c9c1c77530e6c2a80b7eb5b8ea02b4a9cbf649b
Maria Alejandra Murillo <malemurillo15@gmail.com>
2023-10-24 17:13:08 -0500

    Se agrega controlador de tipo de usuario

---
+++ src/AgenciaViajes.API/Controllers/UserTypeController.cs
@@ -0,0 +1,18 @@
+﻿using AgenciaViajes.Application.Features.UserTypeFeatures.Queries.GetUserTypes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgenciaViajes.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserTypeController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<GetUserTypesResponse>>> GetById(
+            [FromServices] IGetUserTypesQuery getUserTypes)
+        {
+            var response = await getUserTypes.Execute();
+            return Ok(response);
+        }
+    }
+}
